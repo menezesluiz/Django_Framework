252ec57e28dc420a80cc75ed9b119d322234b9ed
Luiz Eduard <29684170+menezesluiz@users.noreply.github.com>
2020-09-09 14:45:05 -0300

    Django

--- PyDjango/core/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Produto(models.Model):
-    nome = models.CharField('Nome', max_length=100)
-    preco = models.DecimalField('Preço')
-    estoque = models.IntegerField('Quantidade em Estoque')
